2df208b0d4e54f46882ebdb1594c184dc1050a5d
unknown <bhaleraoshubham99@gmail.com>
2020-08-25 02:19:47 +0530

    Added Shop data to firestore and loaded it into redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import { setCurrentUser } from "./redux/user/user.actions";
 
 import "./App.css";
@@ -12,6 +13,7 @@ import CheckoutPage from "./pages/checkout/checkout.component";
 
 import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { selectCurrentUser } from "./redux/user/user.selectors";
 
 class App extends React.Component {
    unsubscribeFromAuth = null;
@@ -20,7 +22,6 @@ class App extends React.Component {
       const { setCurrentUser } = this.props;
       this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
          if (userAuth) {
-            console.log("User from App Component ", userAuth);
             const userRef = await createUserProfileDocument(userAuth);
             const snapShot = await userRef.get();
             setCurrentUser({
@@ -62,8 +63,8 @@ class App extends React.Component {
    }
 }
 
-const mapStateToProps = (state) => ({
-   currentUser: state.user.currentUser,
+const mapStateToProps = createStructuredSelector({
+   currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
