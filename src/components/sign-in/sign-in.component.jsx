f9d8b61bf9f752e06084561f87f783b806818ac0
unknown <bhaleraoshubham99@gmail.com>
2020-08-31 19:20:17 +0530

    Signin and Signup components now use hooks

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,74 +1,69 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import FormInput from '../form-input/form-input.component';
-import CustomButton from '../custom-button/custom-button.component';
+import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
-import './sign-in.styles.scss';
+import "./sign-in.styles.scss";
 
-class SignIn extends React.Component {
-  constructor(props) {
-    super(props);
+const SignIn = () => {
+   const [userCredentials, setCredentials] = useState({
+      email: "",
+      password: "",
+      error: "",
+   });
+   const { email, password } = userCredentials;
+   const handleSubmit = async (event) => {
+      event.preventDefault();
+      try {
+         await auth.signInWithEmailAndPassword(email, password);
+         setCredentials({ ...userCredentials, email: "", password: "" });
+      } catch (error) {
+         console.log(error);
+         setCredentials({ ...userCredentials, error: error.message });
+      }
+   };
 
-    this.state = {
-      email: '',
-      password: ''
-    };
-  }
+   const handleChange = (event) => {
+      const { value, name } = event.target;
+      setCredentials({ ...userCredentials, [name]: value });
+   };
 
-  handleSubmit = async event => {
-    event.preventDefault();
+   return (
+      <div className="sign-in">
+         <h2>I already have an account</h2>
+         <span>Sign in with your email and password</span>
+         {userCredentials.error && (
+            <p className="error">{userCredentials.error}</p>
+         )}
 
-    const { email, password } = this.state;
-
-    try {
-      await auth.signInWithEmailAndPassword(email, password);
-      this.setState({ email: '', password: '' });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  handleChange = event => {
-    const { value, name } = event.target;
-
-    this.setState({ [name]: value });
-  };
-
-  render() {
-    return (
-      <div className='sign-in'>
-        <h2>I already have an account</h2>
-        <span>Sign in with your email and password</span>
-
-        <form onSubmit={this.handleSubmit}>
-          <FormInput
-            name='email'
-            type='email'
-            handleChange={this.handleChange}
-            value={this.state.email}
-            label='email'
-            required
-          />
-          <FormInput
-            name='password'
-            type='password'
-            value={this.state.password}
-            handleChange={this.handleChange}
-            label='password'
-            required
-          />
-          <div className='buttons'>
-            <CustomButton type='submit'> Sign in </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-              Sign in with Google
-            </CustomButton>
-          </div>
-        </form>
+         <form onSubmit={handleSubmit}>
+            <FormInput
+               name="email"
+               type="email"
+               handleChange={handleChange}
+               value={email}
+               label="email"
+               required
+            />
+            <FormInput
+               name="password"
+               type="password"
+               value={password}
+               handleChange={handleChange}
+               label="password"
+               required
+            />
+            <div className="buttons">
+               <CustomButton type="submit"> Sign in </CustomButton>
+               <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                  Sign in with Google
+               </CustomButton>
+            </div>
+         </form>
       </div>
-    );
-  }
-}
+   );
+};
 
 export default SignIn;
